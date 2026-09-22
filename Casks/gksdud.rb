76405efb1ddd7e28a24cadc9f364ec33fbb8080d
Jake / 제이크 <jake.h@grepp.co>
2026-09-22 03:49:59 +0000

    [CHORE] Update gksdud to 1.3.2

--- Casks/gksdud.rb
+++ Casks/gksdud.rb
@@ -1,6 +1,6 @@
 cask "gksdud" do
-  version "1.3.1"
-  sha256 "873267e04e70876903ee9a16b26b814eb806349a054fa4f55d16544a269d8030"
+  version "1.3.2"
+  sha256 "fa95d249e8f1df20626f8e0410bc67bfef9e6daf0ab64b390991c2fcf16438f4"
 
   url "https://github.com/codingnoye/gksdud/releases/download/v#{version}/gksdud-#{version}-macos-universal.zip"
   name "gksdud"
